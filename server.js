518468d622318b72eb78980aec69fdeef16dc7ca
omi <omi@omis-MacBook-Pro.local>
2017-12-12 12:22:05 +0200

    simplified server.js

--- server.js
+++ server.js
@@ -4,12 +4,4 @@ const port =  process.env.PORT || 3001 ;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get("/api", function(req, res)  { 
-    var json = {
-        "glossary": {}
-    }
-;    
-    res.send(json)
-});
-
 app.listen(port, () => console.log('Example app listening on port '+ port))
